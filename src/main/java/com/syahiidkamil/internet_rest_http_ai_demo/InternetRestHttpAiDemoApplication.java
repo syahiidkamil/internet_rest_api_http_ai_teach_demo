b3b1566d89b6a9e7884e4b5bf9474d7f3f760ade
Syahiid Nur Kamil <syahiidkamil.tech@gmail.com>
2025-04-17 14:56:06 +0700

    first: springboot from https://start.spring.io/

---
+++ src/main/java/com/syahiidkamil/internet_rest_http_ai_demo/InternetRestHttpAiDemoApplication.java
@@ -0,0 +1,22 @@
+package com.syahiidkamil.internet_rest_http_ai_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class InternetRestHttpAiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetRestHttpAiDemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/syahiidkamil/internet_rest_http_ai_demo/InternetRestHttpAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syahiidkamil.internet_rest_http_ai_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetRestHttpAiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
